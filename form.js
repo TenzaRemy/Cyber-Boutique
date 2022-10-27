12474faa55feb76c164f094ef5ab33693460c952
Remy <remy.dufour.etudiant@gmail.com>
2022-10-27 20:47:51 +0200

    ajout validation complète du formulaire verification des champs saisies lors de son envoi et couleur msg err pour accessibilté

--- form.js
+++ form.js
@@ -5,17 +5,20 @@ function validateName() {
   const nameValid = document.getElementById("name").value;
   if(nameValid.length == 0){
     document.getElementById("name").style.borderColor = "red";
+    document.getElementById("name").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("nameError").style.color = "red";
     nameError.innerHTML = 'Votre nom et prénom sont requis';
     return false;
   }
   if(!nameValid.match(/^[A-Za-z\é\è\ê\-]*\s{1}[A-Za-z\é\è\ê\-]*$/)) {
     document.getElementById("name").style.borderColor = "red";
+    document.getElementById("name").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("nameError").style.color = "red";
     nameError.innerHTML = 'Chiffres et caractères spéciaux interdits';
     return false;
   }
   document.getElementById("name").style.borderColor = "#0cd577";
+  document.getElementById("name").style.boxShadow = "inset 0 1px 1px #0cd577";
   document.getElementById("nameError").style.color = "green";
   nameError.innerHTML = 'Données saisies correct !';
   return true;
@@ -25,17 +28,20 @@ function validateEmail() {
   const emailValid = document.getElementById("email").value;
   if(emailValid.length == 0){
     document.getElementById("email").style.borderColor = "red";
+    document.getElementById("email").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("emailError").style.color = "red";
     emailError.innerHTML = 'Veuillez saisir votre adresse mail';
     return false;
   }
   if(!emailValid.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
     document.getElementById("email").style.borderColor = "red";
+    document.getElementById("email").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("emailError").style.color = "red";
-    emailError.innerHTML = "Votre adresse mail n'est pas conforme";
+    emailError.innerHTML = "Votre adresse mail est invalide";
     return false;
   }
   document.getElementById("email").style.borderColor = "#0cd577";
+  document.getElementById("email").style.boxShadow = "inset 0 1px 1px #0cd577";
   document.getElementById("emailError").style.color = "green";
   emailError.innerHTML = 'Adresse mail valide !';
   return true;
@@ -45,17 +51,20 @@ function validateArrive() {
   const arriveValid = document.getElementById("arrive").value;
   if(arriveValid.length == 0){
     document.getElementById("arrive").style.borderColor = "red";
+    document.getElementById("arrive").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("arriveError").style.color = "red";
     arriveError.innerHTML = 'Veuillez nous dire comment êtes vous arrivez ici';
     return false;
   }
   if(!arriveValid.match(/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/)) {
     document.getElementById("arrive").style.borderColor = "red";
+    document.getElementById("arrive").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("arriveError").style.color = "red";
     arriveError.innerHTML = "Votre saisie comporte des caractères spéciaux ou est trop longue";
     return false;
   }
   document.getElementById("arrive").style.borderColor = "#0cd577";
+  document.getElementById("arrive").style.boxShadow = "inset 0 1px 1px #0cd577";
   document.getElementById("arriveError").style.color = "green";
   arriveError.innerHTML = 'Information prise en compte !';
   return true;
@@ -63,16 +72,32 @@ function validateArrive() {
 
 function validateMessage() {
   const messageValid = document.getElementById("message").value;
-  if(!messageValid.match(/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/)) {
+  let minimum = 40;
+  const reste = minimum - messageValid.length;
+
+  if(reste > 0) {
     document.getElementById("message").style.borderColor = "red";
+    document.getElementById("message").style.boxShadow = "inset 0 1px 1px red";
     document.getElementById("messageError").style.color = "red";
-    messageError.innerHTML = "Votre saisie est trop courte ou trop longue";
+    messageError.innerHTML = reste + " caractères manquants";
     return false;
   }
   document.getElementById("message").style.borderColor = "#0cd577";
+  document.getElementById("message").style.boxShadow = "inset 0 1px 1px #0cd577";
   document.getElementById("messageError").style.color = "green";
   messageError.innerHTML = 'Message bien pris merci !';
   return true;
+}
+
+function validateForm() {
+  if(!validateName() || !validateEmail() || !validateArrive()|| !validateMessage()) {
+    swal({
+      title: "Erreur",
+      text: "Une erreur est survenue. Veuillez vérifier vos données saisies dans le formulaire",
+      icon: "error",
+      button: "Fermer",
+    })
+  }
 }
 
 function sendEmail() {
@@ -98,7 +123,7 @@ function sendEmail() {
         title: "Demande envoyée",
         text: "Nous reviendrons vers vous dans les plus brefs délais",
         icon: "success",
-        button: "A bientôt !",
+        button: "Fermer",
       }).then(function() {
         window.location = "index.html";
     }),
